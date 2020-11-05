7140b5008fd64d64ae596f69f524c084ebaf38bd
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2020-11-05 18:41:52 -0500

    Fixed default GitHub repo URL missing in configuration. Updated repo URL for checklist

--- build_configuration.cmd
+++ build_configuration.cmd
@@ -1,3 +1,3 @@
 set READABLE_SCRIPT_NAME="A320 NORMAL CHECKLIST"
 set RELEASE_FILE_NAME_PREFIX="xplane-a320-normal-checklist"
-set GITHUB_REPO_URL="https://github.com/____UPDATE_REPO_URL_in_build_configuration_cmd"
+set GITHUB_REPO_URL="https://github.com/VerticalLongboard/xplane-a320-checklist"
