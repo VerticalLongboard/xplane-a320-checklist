2d276028e54c8199b66e7fab95820f71273840e7
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2020-11-05 15:22:23 -0500

    Updated dev env.

--- build_configuration.cmd
+++ build_configuration.cmd
@@ -1,2 +1,3 @@
 set READABLE_SCRIPT_NAME="A320 NORMAL CHECKLIST"
 set RELEASE_FILE_NAME_PREFIX="xplane-a320-normal-checklist"
+set GITHUB_REPO_URL="https://github.com/____UPDATE_REPO_URL_in_build_configuration_cmd"
