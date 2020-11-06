bd10445c55b20d7d3f26e439e9e0caf2ed8fcdc9
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2020-11-06 09:08:50 -0500

    Minor: Moved test_suite to non-generic version of test_suite.

--- test/test_suite.lua
+++ test/test_suite.lua
@@ -1,13 +1,1 @@
-local luaUnitOutput = require("luaunit_output")
-local luaUnit = require("luaunit")
-local flyWithLuaStub = require("xplane_fly_with_lua_stub")
-local imguiStub = require("imgui_stub")
-
-local a320Checklist = dofile("scripts/A320_checklist.lua")
-flyWithLuaStub:suppressLogMessagesBeginningWith("A320 NORMAL CHECKLIST using '")
-
 require("test_configuration")
-
-local runner = luaUnit.LuaUnit.new()
-runner:setOutput(luaUnitOutput.ColorText)
-os.exit(runner:runSuite())
